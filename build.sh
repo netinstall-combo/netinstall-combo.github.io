5673a8369cb5c48cf696d0d793806850e9ef971b
sulincix <aliriza.keskin@pardus.org.tr>
2025-05-23 13:19:28 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 mkdir build
-# replace this
-cat > build/index.html <<EOF
-<html>
-    <head>
-        <title>Hello World</title>
-    </head>
-    <body>
-        hello world
-    </body>
-</html>
-EOF
+cp -f index.html build/
